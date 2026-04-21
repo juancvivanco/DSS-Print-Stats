c5f9828f07d74efaf11b4e8dfb21d62b04adf4a4
Juan <juan.vivancoperez@formlabs.com>
2026-04-21 07:57:40 -0700

    adjusted failed print conditions

--- stats.py
+++ stats.py
@@ -17,16 +17,26 @@ data['Model list'] = df['Parts'].str.split(', ').apply(lambda x: ['-'.join(name.
 arches = data['No. of Arches'].mean() # Arches per print
 totalArches = data['No. of Arches'].sum()
 printTime = data['Print time (min)'].where(~data['Status'].isin(['ABORTED', 'ERROR'])).mean() # Average print time ignoring prints that were aborted or errored out
-reported = 0 # For print defects reported downstream
-failCount = (data['Status'].isin(['ERROR', 'ABORTED', ''])).sum() + reported # For unfinished as stated in STATUS and downstream defects
+archesPH = totalArches/47 # Throughput 
+reported = 52 # For print defects reported downstream and arches that didn't pass inspection
+checks = 0 # False alarms, manually aborted, etc.
+pDowntime = 0.3 # Time spent doing maintenance per printer in hours
+upDowntime = 0 # Time spent doing unplanned repairs in hours
+allPrints = (data['Status'] != '').sum()
+failCount = (data['Status'].isin(['ERROR', 'ABORTED', ''] or data['Print time (min)']<10)).sum() # For unfinished as stated in STATUS and downstream defects
 finished = (data['Status'] == 'FINISHED').sum() # Prints successfully completed
-failRate = (failCount/(failCount + finished))*100
-uptime = data['Print time (min)'].where(~data['Status'].isin(['ABORTED', 'ERROR'])).sum()/99
+failedArch = (reported/(totalArches + failCount*arches))*100 # Arches that didn't pass inspection
+failRate = (failCount/(allPrints))*100
+
+# Adjust last number here to total number of hours (16.5 per day 80 for a week) being considered for the uptime calculation
+uptime = ((data['Print time (min)'].where(~data['Status'].isin(['ABORTED', 'ERROR'])).sum()/60)/(12*47))*100 # Time actually printing over a full week
 
 # Print out stats
 #print(data.head())
 print(totalArches, 'arches in', finished, 'prints.')
+print('Average of', round(archesPH, 2), 'arches per hour')
 print("Number of arches per print:", round(arches, 2))
 print("Print time in minutes:", round(printTime, 2))
 print('Percentage of failed prints:', round(failRate, 2), "%")
-print(f'{round(uptime, 2)}% percent of work hours were spent printing')
+print('Percentage of failed arches:', round(failedArch, 2), "%")
+print(f'~{round(uptime, 2)}% percent of work hours spent printing')
